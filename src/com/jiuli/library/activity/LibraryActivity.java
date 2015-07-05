3ed5ce4992b20dc4b0ca5999fc83c7a94d4ed4ca
siberiawolf <siberiawolf@126.com>
2015-07-05 12:45:11 +0800

    222

--- src/com/jiuli/library/activity/LibraryActivity.java
+++ src/com/jiuli/library/activity/LibraryActivity.java
@@ -11,7 +11,7 @@ public abstract class LibraryActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		Logger.d("111111111111111");
+		Logger.d("111111111111111罗春华");
 	}
 
 	@Override
